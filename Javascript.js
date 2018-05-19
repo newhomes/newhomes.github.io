5b4165cd922572ff33fd42da5fda21dbe996346f
Lisa <feifei35678@gmail.com>
2018-05-18 17:49:29 -0700

    Real Estate Page active/sold toggle

--- Javascript.js
+++ Javascript.js
@@ -1,4 +1,3 @@
-<script>
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -7,4 +6,31 @@ function myFunction() {
         x.className = "topnav";
     }
 }
-</script>
+
+
+var mode = document.querySelectorAll(".mode");
+var sold = document.querySelectorAll(".soldListingContainer");
+var active = document.querySelectorAll(".activeListingContainer");
+
+init();
+
+function init(){
+  //mode buttons event listener
+  for(var i=0; i<mode.length; i++){
+    mode[i].addEventListener("click", function(){
+      mode[0].classList.remove("selected");
+      mode[1].classList.remove("selected");
+      this.classList.add("selected");
+      for(var a=0; a<sold.length; a++){
+        if(this.textContent === "Sold") {
+          sold[a].classList.remove("hide");
+        } else {
+          sold[a].classList.add("hide");
+        }
+        for(var b=0; b<active.length; b++){
+          active[b].classList.toggle("hide");
+        }
+      }
+    });
+  }
+}
